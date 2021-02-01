c107ac90ba92b507e8ab11d7bacb2d77820d41ee
Ahmet Kutay Karacair <39398963+ahmetkutay@users.noreply.github.com>
2021-02-01 19:11:49 +0300

    useState ve useEffect yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import Accordion from "./compenents/accordion";
+import Search from './compenents/search';
 
-function App() {
+/*const items = [
+  {
+    title: "React Nedir",
+    content: "React  bir frontend javascript frameworküdür.",
+  },
+  {
+    title: "Neden React Kullanırız",
+    content: "Çünkü React geliştiricilerin favori js kütüphanesidir.",
+  },
+  {
+    title: "React Nasıl Kullanırız.",
+    content: "You use React by creating compenents.",
+  },
+];*/
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Search />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/compenents/search.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Search = () => {
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const searchTerm = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+
+    if (term && !results.length) {
+      searchTerm();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          searchTerm();
+        }
+      }, 1000);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <br />
+          <input
+            value={term}
+            className="input"
+            placeholder="Arama istediğiniz kelimeyi giriniz."
+            onChange={(e) => setTerm(e.target.value)}
+          ></input>
+        </div>
+      </div>
+      <div className="ui celled list">{renderedResults}</div>
+    </div>
+  );
+};
+
+export default Search;
